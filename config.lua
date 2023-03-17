7d446eed3e5260e2df8ed44eccf4f22beeedd86e
Art-Wizard <jyry.niemitalo@gmail.com>
2023-03-17 09:29:22 +0200

    configuration files

---
+++ config.lua
@@ -0,0 +1,13 @@
+application = {
+    content = {
+        width = 800,
+        height = 1280, 
+        scale = "letterbox",
+        fps = 60,
+        
+        imageSuffix = {
+            ["@2x"] = 1.5,
+            ["@4x"] = 3.0,
+        },
+    },
+}
